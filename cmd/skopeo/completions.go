8f370680fe952b3fcd695eb7ab1b604029a284f5
Yedaya Katsman <yedaya.ka@gmail.com>
2025-02-16 23:21:04 +0200

    Complete paths for some transports
    Transports that reference a file or directory are completed.
    
    Signed-off-by: Yedaya Katsman <yedaya.ka@gmail.com>

--- cmd/skopeo/completions.go
+++ cmd/skopeo/completions.go
@@ -4,10 +4,47 @@ import (
 	"github.com/containers/image/v5/tarball"
 	"github.com/containers/image/v5/transports"
 	"github.com/spf13/cobra"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
 )
 
-// autocompleteSupportedTransports list all supported transports with the colon suffix.
-func autocompleteSupportedTransports(cmd *cobra.Command, args []string, toComplete string) ([]cobra.Completion, cobra.ShellCompDirective) {
+func autocompleteTransports(cmd *cobra.Command, args []string, toComplete string) ([]cobra.Completion, cobra.ShellCompDirective) {
+	directive := cobra.ShellCompDirectiveNoSpace
+	// We still don't have a transport
+	if !strings.Contains(toComplete, ":") {
+		transports := supportedTransportSuggestions()
+		return transports, directive | cobra.ShellCompDirectiveNoFileComp
+	}
+	if strings.HasPrefix(toComplete, "oci-archive:") || strings.HasPrefix(toComplete, "docker-archive:") || strings.HasPrefix(toComplete, "sif:") || strings.HasPrefix(toComplete, "oci:") {
+		return nil, directive
+	}
+	if toComplete == "docker:" {
+		return []cobra.Completion{"docker://"}, directive | cobra.ShellCompDirectiveNoFileComp
+	}
+	if strings.HasPrefix(toComplete, "dir:") {
+		// just ShellCompDirectiveFilterDirs is more correct, but doesn't work here in bash, see https://github.com/spf13/cobra/issues/2242. Instead we get the directories ourselves.
+		curDir := filepath.Dir(strings.TrimPrefix(toComplete, "dir:"))
+		entries, err := os.ReadDir(curDir)
+		if err != nil {
+			cobra.CompErrorln("Failed ReadDir at " + curDir)
+			// Fallback to whatever the shell gives us
+			return nil, cobra.ShellCompDirectiveFilterDirs | cobra.ShellCompDirectiveNoFileComp
+		}
+		suggestions := make([]cobra.Completion, 0, len(entries))
+		for _, e := range entries {
+			if e.IsDir() {
+				suggestions = append(suggestions, "dir:"+path.Join(curDir, e.Name())+"/")
+			}
+		}
+		return suggestions, cobra.ShellCompDirectiveNoFileComp | cobra.ShellCompDirectiveNoSpace
+	}
+	return nil, directive | cobra.ShellCompDirectiveNoFileComp
+}
+
+// supportedTransportSuggestions list all supported transports with the colon suffix.
+func supportedTransportSuggestions() []string {
 	tps := transports.ListNames()
 	suggestions := make([]cobra.Completion, 0, len(tps))
 	for _, tp := range tps {
@@ -18,5 +55,5 @@ func autocompleteSupportedTransports(cmd *cobra.Command, args []string, toComple
 			suggestions = append(suggestions, tp+":")
 		}
 	}
-	return suggestions, cobra.ShellCompDirectiveNoFileComp | cobra.ShellCompDirectiveNoSpace
+	return suggestions
 }
